bf237b0495385366d303f3f051ddaf92c9ffcad3
ca2roaldsoy <roaldsoyen@gmail.com>
2020-03-17 22:41:23 +0100

    added filter-search

--- src/components/RecipeList.js
+++ src/components/RecipeList.js
@@ -2,7 +2,8 @@ import React, { useState, useEffect } from "react";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Spinner from "react-bootstrap/Spinner";
-import ApiUrl from "../constants/api";
+import { ApiUrl } from "../constants/api";
+import RecipeItem from "./RecipeItem";
 
 function RecipeList() {
   const [recipes, setRecipes] = useState([]);
@@ -12,19 +13,52 @@ function RecipeList() {
   useEffect(() => {
     fetch(ApiUrl)
       .then(response => response.json())
-      .then(data => {
-        setRecipes(data.results);
-        setFilteredRecipes(data.results);
+      .then(json => {
+        setRecipes(json.results);
+        setFilteredRecipes(json.results)
       })
       .catch(error => console.log(error))
       .finally(() => setSpinnner(false));
-  });
+  }, []);
 
   if (spinner) {
     return <Spinner animation="border" className="spinner" />;
   }
 
-  return false;
+  const filterRecipes = function (e) {
+
+    const searchValue = e.target.value.toLowerCase();
+
+    const filteredArr = recipes.filter(function (r) {
+      const lowerCaseName = r.name.toLowerCase();
+
+      if (lowerCaseName.startsWith(searchValue)) {
+        return true;
+      }
+
+      return false;
+
+    });
+
+    setFilteredRecipes(filteredArr)
+
+  };
+
+  return (
+    <>
+      <Row>
+        {filteredRecipes.map(recipe => {
+          const { title, thumbnail } = recipe;
+
+          return (
+            <Col sm={6} md={3}>
+              <RecipeItem title={title} thumbnail={thumbnail} />
+            </Col>
+          );
+        })}
+      </Row>
+    </>
+  );
 }
 
 export default RecipeList;
